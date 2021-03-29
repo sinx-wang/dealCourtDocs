d908301e40f790a2c41d23d4d5049e87fb78ee2b
Sinx Wang <cosx1999@hotmail.com>
2021-03-29 08:24:24 +0800

    wait loading

--- get_data.py
+++ get_data.py
@@ -1,5 +1,8 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import time
 
 
@@ -16,12 +19,13 @@ class GetData:
             "profile.default_content_setting_values.automatic_downloads": 1
         }
         options.add_experimental_option('prefs', prefs)
+        self.timeout = 30
         self.driver = webdriver.Chrome(
             executable_path='C:\\Users\\cosx1\\Downloads\\chromedriver.exe',
             service_log_path=os.devnull,
             options=options)
         self.driver.maximize_window()
-        self.driver.set_page_load_timeout(30)
+        self.driver.set_page_load_timeout(self.timeout)
         self.driver.get(self.url)
 
     def login(self):
@@ -30,7 +34,9 @@ class GetData:
 
         # 登录
         iframe = self.driver.find_element_by_id('contentIframe')
-        self.driver.switch_to.frame(iframe)
+        WebDriverWait(self.driver, self.timeout).until(
+            EC.frame_to_be_available_and_switch_to_it(iframe))
+        # self.driver.switch_to.frame(iframe)
         username_input = self.driver.find_element_by_name('username')
         username_input.send_keys('18851821816')
         time.sleep(3)
@@ -45,12 +51,15 @@ class GetData:
 
         search_input = self.driver.find_element_by_xpath(
             '//*[@id="_view_1540966814000"]/div/div[1]/div[2]/input')
+        WebDriverWait(self.driver, self.timeout).until(EC.visibility_of_element_located(search_input))
         search_input.click()
         search_input.send_keys('贵阳银行')
         self.driver.find_element_by_xpath(
             '//*[@id="_view_1540966814000"]/div/div[1]/div[3]').click()
 
+        time.sleep(2)
+
 
 if __name__ == '__main__':
     GET_DATA = GetData()
-    GET_DATA.login()
+    GET_DATA.get_data()
